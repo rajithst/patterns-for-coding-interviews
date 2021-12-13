18763bb1e603adf0354321427b74dd19c5a7fa10
rajithst <rajiththennakoonshady@gmail.com>
2021-12-13 21:50:31 +0900

    DFS - diameter of binary tree  clean dfs implementation

--- tree-depth-first-search/tree_diameter.py
+++ tree-depth-first-search/tree_diameter.py
@@ -4,26 +4,20 @@ class TreeNode:
         self.right, self.left = None, None
 
 
-class Solution:
-    def __init__(self):
-        self.diameter = 0
+def find_diameter(root):
+    diameter = 0
 
-    def find_diameter(self, root):
-        self.calculate_diameter(root)
-        return self.diameter
-
-    def calculate_diameter(self, current_node):
+    def calculate_diameter(current_node):
+        nonlocal diameter
         if current_node is None:
             return 0
 
-        left_height = self.calculate_diameter(current_node.left)
-        right_height = self.calculate_diameter(current_node.right)
-
-        if current_node.left is not None and current_node.right is not None:
-            current_diameter = left_height + right_height + 1
-            self.diameter = max(current_diameter, self.diameter)
-
+        left_height = calculate_diameter(current_node.left)
+        right_height = calculate_diameter(current_node.right)
+        current_diameter = left_height + right_height + 1
+        diameter = max(current_diameter, diameter)
         return max(left_height, right_height) + 1
+    return calculate_diameter(root)
 
 
 root = TreeNode(1)
@@ -32,6 +26,4 @@ root.right = TreeNode(3)
 root.left.left = TreeNode(4)
 root.right.left = TreeNode(5)
 root.right.right = TreeNode(6)
-solution = Solution()
-
-print("Tree Diameter: " + str(solution.find_diameter(root)))
+print(find_diameter(root))
