e36b2c55929b542d05471b3c86063ce83d6c0089
rajithst <rajiththennakoonshady@gmail.com>
2022-01-03 20:02:58 +0900

    top-k elements update with question image

--- top_k_elements/top_k_numbers.py
+++ top_k_elements/top_k_numbers.py
@@ -3,9 +3,11 @@ import heapq as heap
 arr = [3, 1, 5, 12, 2, 11]
 K = 3
 minheap = []
+#insert k elements to minheap
 for i in range(K):
     heap.heappush(minheap, arr[i])
 
+#for rest of the numbers,check if number is larger than minimum value and replace
 for i in range(K, len(arr)):
     if arr[i] > minheap[0]:
         heap.heappop(minheap)
