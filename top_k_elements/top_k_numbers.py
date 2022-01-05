4b20f3acbf09525b40109db4a60655f24758bced
rajithst <rajiththennakoonshady@gmail.com>
2022-01-05 21:21:09 +0900

    top-k elements - reorganize string such a way that no two same characters come next to each other using maxheap

--- top_k_elements/top_k_numbers.py
+++ top_k_elements/top_k_numbers.py
@@ -7,7 +7,8 @@ minheap = []
 for i in range(K):
     heap.heappush(minheap, arr[i])
 
-#for rest of the numbers,check if number is larger than minimum value and replace
+#for rest of the numbers,check if number is larger than minimum value
+#if value is larger than minimum value,replace
 for i in range(K, len(arr)):
     if arr[i] > minheap[0]:
         heap.heappop(minheap)
